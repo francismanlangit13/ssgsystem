fd4e88a6393ab3e57d55fe24574d68c2137170e3
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-05-03 23:09:14 +0800

    rename old and add new final DB

---
+++ database/NEW ssgsystem.sql
@@ -0,0 +1,392 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2023 at 05:07 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ssgsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
+
+CREATE TABLE `activity` (
+  `activity_id` int(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `activity_title` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcement`
+--
+
+CREATE TABLE `announcement` (
+  `announcement_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `announcement_title` varchar(255) NOT NULL,
+  `announcement_body` text NOT NULL,
+  `date_start` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fines_transaction`
+--
+
+CREATE TABLE `fines_transaction` (
+  `transaction_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fines_fee` int(100) NOT NULL,
+  `fines_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `liquidation`
+--
+
+CREATE TABLE `liquidation` (
+  `liqui_id` int(11) NOT NULL,
+  `liqui_name` varchar(255) NOT NULL,
+  `liqui_purpose` text NOT NULL,
+  `liqui_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `referencenumber` varchar(255) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qrcode`
+--
+
+CREATE TABLE `qrcode` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `fee_qr` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `qrcode`
+--
+
+INSERT INTO `qrcode` (`id`, `name`, `fee_qr`, `date`, `status`) VALUES
+(1, 'Dan Kenneth', '', '2023-02-08 00:00:00', 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ssg_expenses`
+--
+
+CREATE TABLE `ssg_expenses` (
+  `expense_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `purpose` varchar(255) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ssg_expenses`
+--
+
+INSERT INTO `ssg_expenses` (`expense_id`, `user_id`, `purpose`, `amount`, `date`) VALUES
+(1, 1, 'Stage Decoration', '500.00', '2023-02-08 15:10:40'),
+(2, 1, '4x4 Wood', '100.00', '2023-02-09 03:15:19'),
+(3, 1, 'Kahoy', '600.00', '2023-02-09 03:25:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `mname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `suffix` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `student_id` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `penalty` varchar(255) NOT NULL,
+  `balance` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `user_type` int(11) NOT NULL,
+  `user_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `fname`, `mname`, `lname`, `suffix`, `email`, `phone`, `password`, `student_id`, `level`, `penalty`, `balance`, `photo`, `user_type`, `user_status`) VALUES
+(1, 'User', '', 'Admin', '', 'admin@gmail.com', '', '0192023a7bbd73250516f069df18b500', '', '', '', '', '', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_status`
+--
+
+CREATE TABLE `user_status` (
+  `user_status_id` int(11) NOT NULL,
+  `user_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_status`
+--
+
+INSERT INTO `user_status` (`user_status_id`, `user_status`) VALUES
+(1, 'Active'),
+(2, 'In Active'),
+(3, 'Archive');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_type`
+--
+
+CREATE TABLE `user_type` (
+  `user_type_id` int(11) NOT NULL,
+  `user_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_type`
+--
+
+INSERT INTO `user_type` (`user_type_id`, `user_type`) VALUES
+(1, 'Admin'),
+(2, 'President'),
+(3, 'Vice President'),
+(4, 'Secretary'),
+(5, 'Treasurer'),
+(6, 'Student'),
+(7, 'Parent');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`activity_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`announcement_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `fines_transaction`
+--
+ALTER TABLE `fines_transaction`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `user_id` (`user_id`) USING BTREE;
+
+--
+-- Indexes for table `liquidation`
+--
+ALTER TABLE `liquidation`
+  ADD PRIMARY KEY (`liqui_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`) USING BTREE;
+
+--
+-- Indexes for table `qrcode`
+--
+ALTER TABLE `qrcode`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ssg_expenses`
+--
+ALTER TABLE `ssg_expenses`
+  ADD PRIMARY KEY (`expense_id`),
+  ADD KEY `user_id` (`user_id`) USING BTREE;
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_type` (`user_type`,`user_status`),
+  ADD KEY `user_status` (`user_status`);
+
+--
+-- Indexes for table `user_status`
+--
+ALTER TABLE `user_status`
+  ADD PRIMARY KEY (`user_status_id`);
+
+--
+-- Indexes for table `user_type`
+--
+ALTER TABLE `user_type`
+  ADD PRIMARY KEY (`user_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `activity_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `announcement`
+--
+ALTER TABLE `announcement`
+  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fines_transaction`
+--
+ALTER TABLE `fines_transaction`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `liquidation`
+--
+ALTER TABLE `liquidation`
+  MODIFY `liqui_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `qrcode`
+--
+ALTER TABLE `qrcode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ssg_expenses`
+--
+ALTER TABLE `ssg_expenses`
+  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_status`
+--
+ALTER TABLE `user_status`
+  MODIFY `user_status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_type`
+--
+ALTER TABLE `user_type`
+  MODIFY `user_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activity`
+--
+ALTER TABLE `activity`
+  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD CONSTRAINT `announcement_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `fines_transaction`
+--
+ALTER TABLE `fines_transaction`
+  ADD CONSTRAINT `fines_transaction_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `ssg_expenses`
+--
+ALTER TABLE `ssg_expenses`
+  ADD CONSTRAINT `ssg_expenses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `user_type` (`user_type_id`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`user_status`) REFERENCES `user_status` (`user_status_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
